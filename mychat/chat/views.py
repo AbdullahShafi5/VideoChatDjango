4f9f453b721eb68f500110fd5ebc4fd14e40a5f5
abdullaSafi <abdullashafi5@gmail.com>
2022-02-14 01:15:20 +0600

    fixing user not showing

--- mychat/chat/views.py
+++ mychat/chat/views.py
@@ -33,20 +33,23 @@ def getToken(request):
 @csrf_exempt
 def createMember(request):
     data = json.loads(request.body)
-    member ,created = RoomMember.objects.get_or_create(
-        name= data['name'],
-        uid = data['UID'],
-        room_name =data['room_name']
+    member, created = RoomMember.objects.get_or_create(
+        name=data['name'],
+        uid=data['UID'],
+        room_name=data['room_name']
     )
-    return JsonResponse({'name':data['name']},safe=False)
+
+    return JsonResponse({'name':data['name']}, safe=False)
+
 
 def getMember(request):
     uid = request.GET.get('UID')
-    room_name = request.GET.get('uid')
+    room_name = request.GET.get('room_name')
+
     member = RoomMember.objects.get(
-        uid= uid,
-        room_name = room_name,
-    )   
+        uid=uid,
+        room_name=room_name,
+    )
     name = member.name
     return JsonResponse({'name':member.name}, safe=False)
 
@@ -54,10 +57,9 @@ def getMember(request):
 def deleteMember(request):
     data = json.loads(request.body)
     member = RoomMember.objects.get(
-        name= data['name'],
-        uid= data['UID'],
-        room_name =data['room_name'],
+        name=data['name'],
+        uid=data['UID'],
+        room_name=data['room_name']
     )
     member.delete()
-    
-    return JsonResponse('Member was deleted',safe=False)
+    return JsonResponse('Member deleted', safe=False)
--- mychat/mychat/settings.py
+++ mychat/mychat/settings.py
@@ -55,7 +55,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mychat.wsgi.application'
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
